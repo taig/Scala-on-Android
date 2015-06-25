742346406032708a8788b90ebaf8d32ac57f6d72
Niklas Klein <mail@taig.io>
2015-06-25 17:26:06 +0200

    Convert UTC time to local format, fixes #3

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,9 @@
-require( [ 'include/main', 'util/main' ], function() {} );
+require( [ 'jquery', 'include/main', 'util/main' ], function( $ )
+{
+	// Replace UTC time string with local date format
+	$( '.meta .date' ).each( function()
+	{
+		var date = new Date( $( this ).html() );
+		$( this ).html( date.toLocaleString() );
+	} );
+} );
