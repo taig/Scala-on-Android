f06f3ce5d1bdd7bf70aad4c26e729059e0ff3b11
Niklas Klein <mail@taig.io>
2015-08-29 13:30:41 +0200

    Fix library project setup

--- src/main/page/library-project/configuration.scala
+++ src/main/page/library-project/configuration.scala
@@ -1,17 +1,11 @@
 import android.Plugin._
 
 object	Build
-extends	android.AutoBuild
+extends	sbt.Buil
 {
-	// Use settings = androidBuildApklib for apklib, respectively
-	val aar = Project( "my-aar-project", file( "." ), settings = androidBuildAar )
+	val main = Project( "my-aar-project", file( "." ), settings = androidBuildAar )
 		.settings(
 			organization := "com.example",
-			// @see https://github.com/pfn/android-sdk-plugin/issues/88
-			sourceGenerators in Compile <<= ( sourceGenerators in Compile )( generators =>
-			{
-				Seq( generators.last )
-			} ),
 			version := "1.0.0"
 		)
 }
