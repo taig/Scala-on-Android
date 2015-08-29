8406574a78baaf017879cce320006052d6e85bdf
Niklas Klein <mail@taig.io>
2015-08-29 13:25:25 +0200

    Upgrade to android-sdk-plugin 1.4.12

--- src/main/software.js
+++ src/main/software.js
@@ -37,7 +37,7 @@ var software =
 	{
 		group: 'com.hanhuy.sbt',
 		artifact: 'android-sdk-plugin',
-		version: '1.4.8',
+		version: '1.4.12',
 		title: names.name( 'sbt-plugin' ),
 		url: function() { return 'https://bintray.com/pfn/sbt-plugins/' + this.artifact + '/'; },
 		format: function()
